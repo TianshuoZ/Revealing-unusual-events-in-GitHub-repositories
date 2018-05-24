094243e5888a6c22b0869105e8d57a1b2c6f09ed
XiyuZhang <37031376+XiyuZhang@users.noreply.github.com>
2018-05-24 23:12:04 +0930

    Version 1.1 (Fetching data & storing into DB)
    Basing on the authorization Login, I connected it with our database. It also can be able to fetch data from GitHub via its API. I only focus on the six most useful types of unusual events and there still is something that not satisfied by myself such as authorization login can be able to get 30 requests per minute (10 requests per minute without authorization login). For this reason, I am trying to find a method to break the limitation, if I need to fetch data when it should be requested more than 30 times. Recently, it seems can be solved by page demanding. In addition, fetching Other data will be added in future.

---
+++ Project Code/Authorization_Login_JAVA/src/main/java/unusualevent/login/info/FetchUserInfo.java
@@ -0,0 +1,18 @@
+package unusualevent.login.info;
+
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.core.JsonParser;
+
+
+
+public class FetchUserInfo {
+	
+	private String login;
+	private String id;
+
+
+	
+	}
+
+
--- Project Code/Authorization_Login_JAVA/src/main/java/unusualevents/login/web/LoginController.java
+++ Project Code/Authorization_Login_JAVA/src/main/java/unusualevents/login/web/LoginController.java
@@ -1,8 +1,20 @@
 package unusualevents.login.web;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+
+import unusualevents.db.Service.userTable;
+import unusualevents.json.service.urlToJson;
 import unusualevents.login.config.Config;
 import unusualevents.login.utils.RemoteOauthRequest;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -16,6 +28,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 
     private Logger logger = LoggerFactory.getLogger(LoginController.class);
+    
+    @GetMapping("login")
+    public String login(Model model) {
+        model.addAttribute("login", "login");
+        return "login/login";
+    }
 
     @GetMapping("login/github")
     public String loginGithub(String code, Model model) {
@@ -34,17 +52,63 @@ public class LoginController {
         model.addAttribute("result1", jsonObject);
         
         //-----------------
-        String token= jsonObject.get("access_token").toString();
+        String token = jsonObject.get("access_token").toString();
         String url2 = Config.GITHUB_API_URL + token;
         String userinfo = RemoteOauthRequest.get(url2);
         JSONObject jsonUserI = JSONObject.parseObject(userinfo);
-        System.out.println("jsonUserI:" + jsonUserI);
-//        for(int i=0;i<jsonUserI.size();i++)
-//        {
-//        	System.out.println(jsonUserI)
-//        	
-//        }
-//        
+        
+        //System.out.println(jsonUserI.getString("login"));
+        //System.out.println(jsonUserI.getString("id"));
+        //System.out.println(jsonUserI.getString("avatar_url"));
+        
+        String id = jsonUserI.getString("id");
+        String username = jsonUserI.getString("login");
+        String avatar_url = jsonUserI.getString("avatar_url");
+        String repository_number = jsonUserI.getString("public_repos");
+        
+        String a = "https://api.github.com/repos/XiyuZhang/Revealing-unusual-events-in-GitHub-repositories/commits";
+        String b = "59dbf675c23438fddb707d661e79617c7df910f5";
+        
+        urlToJson reposUrl = new urlToJson();
+        
+        JSONObject reposJson = reposUrl.toJson(a+"/"+b);
+        System.out.println(reposJson.toString());
+        JSONObject stats = JSONObject.parseObject(reposJson.get("stats").toString());
+        System.out.println(stats.get("total").toString());
+        System.out.println(stats.get("additions").toString());
+        System.out.println(stats.get("deletions").toString());
+   //     JSONArray reposJson = reposUrl.toJsonArray(jsonUserI.getString("repos_url"));
+        
+       
+        /*
+        System.out.println("-1-------------------------------"); 
+        System.out.println(reposJson.get(0).toString()); 
+        
+        System.out.println("-2-------------------------------"); 
+        JSONObject test = JSONObject.parseObject(reposJson.get(0).toString());
+        System.out.println(test.toJSONString());
+        System.out.println(test.get("owner").toString());
+        System.out.println("-3-------------------------------"); 
+        JSONObject owner = JSONObject.parseObject(test.get("owner").toString());
+        System.out.println(owner.get("gists_url"));
+        */
+        
+        
+
+ //--------------------add user table     
+ /*
+        userTable adduser = new userTable();
+        if(adduser.addUser(id, username, avatar_url, repository_number)==1)
+        {
+        	System.out.println("Add user in table successfully"); 
+        }else
+        	System.out.println("Add user in table unsuccessfully");
+//--------------------add user table*/
+        
+//========================================================================================
+        
+//        System.out.println("jsonUserI:" + jsonUserI.toString());
+        
         model.addAttribute("result2", userinfo);
 
         DigestUtils.md5DigestAsHex(userinfo.getBytes());
@@ -52,6 +116,15 @@ public class LoginController {
         return "login/success";
     }
 
+    
+
+    
+    
+    
+    
+    
+    
+    
     
  //-----------------------------------------------------------------------------------------   
     @GetMapping("user/info")
@@ -67,3 +140,31 @@ public class LoginController {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
