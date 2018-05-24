c1e7ef019cbaa073d866f99b413f390062a5509a
XiyuZhang <37031376+XiyuZhang@users.noreply.github.com>
2018-05-24 23:14:34 +0930

    Delete Config.java

--- Project Code/Authorization_Login_JAVA/Authorization_Login_JAVA/src/main/java/unusualevents/login/config/Config.java
+++
@@ -1,9 +0,0 @@
-package unusualevents.login.config;
-
-public class Config {
-	public static final String CLIENT_ID="780beb2416884df325d1";
-    public static final String CLIENT_SECRET="c125884f9f1c204ccbddd55d3f39b55e8ead72bd";
-    public static final String OAUTH_URL = "http://github.com/login/oauth/authorize";
-    public static final String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
-    public static final String GITHUB_API_URL = "https://api.github.com/user?access_token=";
-}
